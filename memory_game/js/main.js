7a492b68086208e0d17ddf4c1e6b84a078d5f380
Christopher Bailey <chris@Christophers-MacBook-Pro.local>
2018-03-07 19:22:06 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,31 @@
 console.log("Up and Running!");
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "King";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+		
+	}
+}
+
 
