df71a2c00c9a7f98a9c5a98a7488e3d208f5491e
Christopher Bailey <chris@Christophers-MacBook-Pro.local>
2018-03-11 19:02:36 -0400

    added checkForMatch, flipCard, createBoard

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,36 +22,33 @@ var cards = [
 ];
 
 var cardsInPlay = [];
-
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
-
+    alert("Sorry, try again.");
   }
 };
 
-var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
   cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardImage)
-  console.log(cards[cardId].suit)
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     checkForMatch();
     cardsInPlay = [];
   }
 };
 
-flipCard(0);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
 
-flipCard(3);
-
-
-
-
-
-
-
-
-
+createBoard();
